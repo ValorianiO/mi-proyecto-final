1171c20dc9a5baa8ca47cfd4c28d0fee42a8fc7a
Osvaldo Valoriani <osvaldovaloriani@hotmail.com>
2022-11-09 19:18:27 -0300

    agrego templates saludar,saludar a, mostrar notas, imc

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "ejemplo/saludar.html")
+    return render(request, "ejemplo/saludar.html", {"nombre": "Osvaldo"})
 
+def index_dos(request, nombre, apellido):
+    return render(request, "ejemplo/saludar.html", 
     
+    {
+           "nombre": nombre, 
+          "apellido": apellido, 
+          
+          })
+
+def index_tres(request):
+    return render (request, "ejemplo/saludar.html", 
+    {"notas": [1,2,3,4,5,6,7,8]}
+    )
+
+def imc(request, peso, altura):
+    imc= 1
+    return render(request, "ejemplo/imc.html", {"imc": imc})
+
