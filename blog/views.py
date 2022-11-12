3cf449560657eb1050ca084ac7c9c8c41ba458e4
Osvaldo Valoriani <osvaldovaloriani@hotmail.com>
2022-11-12 16:56:49 -0300

    agregar campos al usuario, mostrar contenido de post en index

--- blog/views.py
+++ blog/views.py
@@ -9,17 +9,18 @@ from django.views import View
 from django.views.generic import ListView, CreateView, DeleteView, UpdateView, DetailView
 from blog.models import Post
 
-@login_required
 def index(request):
-    return render(request, 'blog/index.html')
-class ListPost(LoginRequiredMixin, ListView):
+    posts = Post.objects.order_by('-date_published').all()
+    return render(request, 'blog/index.html', {"posts": posts})
+
+class ListPost(ListView):
     model=Post
 
 class CreatePost(CreateView):
     model=Post
     fields = ['title', 'short_content', 'content', 'image']
     success_url = reverse_lazy("list-post")
-
+    
 class DetailPost(DetailView):
     model=Post
 
@@ -31,16 +32,19 @@ class UpdatePost(UpdateView):
 class DeletePost(DeleteView):
     model=Post
     success_url = reverse_lazy("list-post")
+
 class SearchPostByName(ListView):
     def get_queryset(self):
         blog_title = self.request.GET.get('post-title')
         return Post.objects.filter(title__icontains=blog_title)
+
 class BlogLogin(LoginView):
     template_name = 'blog/blog_login.html'
     next_page = reverse_lazy("list-post")
+
 class BlogLogout(LogoutView):
     template_name = 'blog/blog_logout.html'
-
+    
 class BlogSignUp(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("blog-login")
@@ -48,6 +52,7 @@ class BlogSignUp(CreateView):
 
 class ProfileUpdate(UpdateView):
     model = User
-    fields = ['username']
+    fields = ['username', 'first_name', 'last_name', 'email']
     success_url = reverse_lazy("blog-login")
 
+    
